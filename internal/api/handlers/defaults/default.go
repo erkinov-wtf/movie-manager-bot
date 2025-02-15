0dc24b0308eb44efc05b3f90af8a3fb173f571fc
Rakhmatilla Erkinov <erkinovit@gmail.com>
2025-02-15 21:17:54 +0500

    refactor: Inject dependencies into DefaultHandler
    - Updated DefaultHandler to receive dependencies via struct.
    - Modified NewDefaultHandler to initialize with dependencies.
    - Injected database, config, TMDB client, and cache.
    - Updated Start command to use injected database.
    - Updated GetToken command to use injected cache.
    - Updated HandleReplySearch to use injected cache.
    - Updated HandleTokenReply to use injected database.

--- internal/api/handlers/defaults/default.go
+++ internal/api/handlers/defaults/default.go
@@ -7,8 +7,7 @@ import (
 	"github.com/erkinov-wtf/movie-manager-bot/internal/api/handlers/tv"
 	"github.com/erkinov-wtf/movie-manager-bot/internal/models"
 	"github.com/erkinov-wtf/movie-manager-bot/internal/storage/cache"
-	"github.com/erkinov-wtf/movie-manager-bot/internal/storage/database"
-	keyboards2 "github.com/erkinov-wtf/movie-manager-bot/pkg/keyboards"
+	"github.com/erkinov-wtf/movie-manager-bot/pkg/keyboards"
 	"github.com/erkinov-wtf/movie-manager-bot/pkg/messages"
 	"github.com/erkinov-wtf/movie-manager-bot/pkg/utils"
 	"gopkg.in/telebot.v3"
@@ -18,11 +17,11 @@ import (
 	"strings"
 )
 
-func (*DefaultHandler) Start(context telebot.Context) error {
+func (h *DefaultHandler) Start(context telebot.Context) error {
 	log.Print(messages.StartCommand)
 
 	var existingUser models.User
-	if err := database.DB.Where("id = ?", context.Sender().ID).First(&existingUser).Error; err != nil {
+	if err := h.Database.Where("id = ?", context.Sender().ID).First(&existingUser).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			btn := &telebot.ReplyMarkup{}
 			btnRows := []telebot.Row{
@@ -43,7 +42,7 @@ func (*DefaultHandler) Start(context telebot.Context) error {
 			return context.Send(messages.InternalError)
 		}
 	} else {
-		menu := keyboards2.LoadMenuKeyboards(context.Bot())
+		menu := keyboards.LoadMenuKeyboards(context.Bot())
 		err = context.Send(messages.UseHelp, telebot.ModeMarkdown, menu)
 		if err != nil {
 			log.Print(err)
@@ -64,18 +63,18 @@ func (h *DefaultHandler) handleStartCallback(context telebot.Context) error {
 		TmdbApiKey: nil,
 	}
 
-	if err := database.DB.Create(&newUser).Error; err != nil {
+	if err := h.Database.Create(&newUser).Error; err != nil {
 		log.Printf("cant create user: %v", err.Error())
 		return context.Send(messages.InternalError)
 	}
 
-	keyboard := keyboards2.LoadTokenRegistrationKeyboard(context.Bot(), h)
+	keyboard := keyboards.LoadTokenRegistrationKeyboard(context.Bot(), h)
 	return context.Send(messages.Registered, keyboard, telebot.ModeMarkdown)
 }
 
 func (h *DefaultHandler) GetToken(context telebot.Context) error {
 	userId := context.Sender().ID
-	isActive, userCache := cache.UserCache.Get(userId)
+	isActive, userCache := h.Cache.UserCache.Get(userId)
 
 	if isActive && !userCache.ApiToken.IsTokenWaiting {
 		return context.Send(messages.TokenAlreadyExists, telebot.ModeMarkdown)
@@ -85,6 +84,7 @@ func (h *DefaultHandler) GetToken(context telebot.Context) error {
 }
 
 func (h *DefaultHandler) HandleReplySearch(context telebot.Context, userCache *cache.UserCacheItem) error {
+	h.Cache.UserCache.SetSearchStartFalse(context.Sender().ID)
 	if userCache.SearchState.IsTVShowSearch {
 		return h.handleTVShowSearch(context)
 	}
@@ -92,13 +92,11 @@ func (h *DefaultHandler) HandleReplySearch(context telebot.Context, userCache *c
 }
 
 func (h *DefaultHandler) handleTVShowSearch(context telebot.Context) error {
-	cache.UserCache.SetSearchStartFalse(context.Sender().ID)
 	tvHandler := &tv.TVHandler{}
 	return tvHandler.SearchTV(context)
 }
 
 func (h *DefaultHandler) handleMovieSearch(context telebot.Context) error {
-	cache.UserCache.SetSearchStartFalse(context.Sender().ID)
 	movieHandler := &movie.MovieHandler{}
 	return movieHandler.SearchMovie(context)
 }
@@ -111,15 +109,15 @@ func (h *DefaultHandler) HandleTextInput(context telebot.Context) error {
 		return context.Send(messages.TokenTestFailed, telebot.ModeMarkdown)
 	}
 
-	if err := database.DB.Model(&models.User{}).
+	if err := h.Database.Model(&models.User{}).
 		Where("id = ?", userId).
 		Update("tmdb_api_key", inputText).Error; err != nil {
 		log.Print(err)
 		return context.Send(messages.InternalError)
 	}
 
-	cache.UserCache.UpdateTokenState(userId, false)
-	menu := keyboards2.LoadMenuKeyboards(context.Bot())
+	h.Cache.UserCache.UpdateTokenState(userId, false)
+	menu := keyboards.LoadMenuKeyboards(context.Bot())
 
 	return context.Send(messages.TokenSaved, menu, telebot.ModeMarkdown)
 }
--- internal/api/handlers/defaults/type.go
+++ internal/api/handlers/defaults/type.go
@@ -2,15 +2,25 @@ package defaults
 
 import (
 	"github.com/erkinov-wtf/movie-manager-bot/internal/api/interfaces"
+	"github.com/erkinov-wtf/movie-manager-bot/internal/config"
 	"github.com/erkinov-wtf/movie-manager-bot/internal/config/app"
+	"github.com/erkinov-wtf/movie-manager-bot/internal/storage/cache"
+	"github.com/erkinov-wtf/movie-manager-bot/internal/tmdb"
+	"gorm.io/gorm"
 )
 
 type DefaultHandler struct {
-	App *app.App
+	Cfg        *config.Config
+	Database   *gorm.DB
+	TMDBClient *tmdb.Client
+	Cache      *cache.Manager
 }
 
 func NewDefaultHandler(app *app.App) interfaces.DefaultInterface {
 	return &DefaultHandler{
-		App: app,
+		Cfg:        app.Cfg,
+		Database:   app.Database,
+		TMDBClient: app.TMDBClient,
+		Cache:      app.Cache,
 	}
 }
