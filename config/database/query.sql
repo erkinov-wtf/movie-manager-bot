ea2dea9a725d439d3ff522bd1ac5ca5338f18ae3
Rakhmatilla Erkinov <erkinovit@gmail.com>
2025-03-10 20:42:44 +0500

    refactor: Improve database queries for users, TV shows, movies, and watchlists
    - Updates user queries to fetch specific fields.
    - Adds new queries to check existence of users.
    - Introduces soft delete functionality for TV shows and movies.
    - Modifies TV show queries to fetch specific fields.
    - Implements movie existence checks.
    - Updates watchlist queries to include soft delete logic.
    - Add new query for get user TMDB Key.
    - Add user exists check for movies, tv shows and watchlists.

--- config/database/query.sql
+++ config/database/query.sql
@@ -1,56 +1,83 @@
 /* Users Table */
 
 -- name: GetUser :one
-SELECT * FROM users
+SELECT id, first_name, last_name, username, language, tmdb_api_key, created_at, updated_at
+FROM users
 WHERE id = $1 LIMIT 1;
 
--- name: CountUsers :one
-SELECT count(*) FROM users
-WHERE id = $1;
+-- name: GetUsers :many
+SELECT id, first_name, last_name, username, language, created_at, updated_at
+FROM users;
+
+-- name: UserExists :one
+SELECT EXISTS(SELECT 1 FROM users WHERE id = $1);
 
 -- name: CreateUser :exec
 INSERT INTO users (first_name, last_name, username, language, tmdb_api_key)
-VALUES ($1, $2, $3, $4, $4);
+VALUES ($1, $2, $3, $4, $5);
 
 -- name: UpdateUserTMDBKey :exec
 UPDATE users
 SET tmdb_api_key = $2
 WHERE id = $1;
 
+-- name: GetUserTMDBKey :one
+SELECT tmdb_api_key FROM users
+WHERE id = $1 LIMIT 1;
+
 
 /* TV Shows Table */
 
 -- name: GetUserTVShows :many
-SELECT * FROM tv_shows
-WHERE user_id = $1;
+SELECT id, api_id, name, seasons, episodes, runtime, status, created_at, updated_at
+FROM tv_shows
+WHERE user_id = $1 AND deleted_at IS NULL;
 
 -- name: GetWatchedSeasons :one
 SELECT seasons FROM tv_shows
-WHERE api_id = $1 AND user_id = $2;
+WHERE api_id = $1 AND user_id = $2 AND deleted_at IS NULL;
+
+-- name: TVShowExists :one
+SELECT EXISTS(SELECT 1 FROM tv_shows WHERE api_id = $1 AND user_id = $2 AND deleted_at IS NULL);
 
 -- name: CreateTVShow :exec
 INSERT INTO tv_shows (user_id, api_id, name, seasons, episodes, runtime, status)
-VALUES ($1, $2, $3, $4, $5,$6, $7);
+VALUES ($1, $2, $3, $4, $5, $6, $7);
 
 -- name: UpdateTVShow :exec
 UPDATE tv_shows
-SET (seasons = $2, episodes = $3, runtime = $4)
-WHERE api_id = $1;
+SET seasons = $3, episodes = $4, runtime = $5
+WHERE api_id = $1 AND user_id = $2 AND deleted_at IS NULL;
+
+-- name: SoftDeleteTVShow :exec
+UPDATE tv_shows
+SET deleted_at = NOW()
+WHERE api_id = $1 AND user_id = $2 AND deleted_at IS NULL;
 
 
 /* Movies Table */
 
 -- name: GetUserMovies :many
-SELECT * FROM movies
-WHERE user_id = $1;
+SELECT id, api_id, title, runtime, created_at, updated_at
+FROM movies
+WHERE user_id = $1 AND deleted_at IS NULL;
 
--- name: CountUserMovies :one
-SELECT count(*) FROM movies
-WHERE api_id = $1 AND user_id = $2;
+-- name: MovieExists :one
+SELECT EXISTS(SELECT 1 FROM movies WHERE api_id = $1 AND user_id = $2 AND deleted_at IS NULL);
 
 -- name: CreateMovie :exec
-INSERT INTO movies (user_id, api_id, title, runtime, status)
-VALUES ($1, $2, $3, $4, $5);
+INSERT INTO movies (user_id, api_id, title, runtime)
+VALUES ($1, $2, $3, $4);
+
+-- name: UpdateMovie :exec
+UPDATE movies
+SET runtime = $3, title = $4
+WHERE api_id = $1 AND user_id = $2 AND deleted_at IS NULL;
+
+-- name: SoftDeleteMovie :exec
+UPDATE movies
+SET deleted_at = NOW()
+WHERE api_id = $1 AND user_id = $2 AND deleted_at IS NULL;
 
 
 /* Watchlists Table */
@@ -59,14 +86,26 @@ VALUES ($1, $2, $3, $4, $5);
 INSERT INTO watchlists (user_id, show_api_id, type, title, image)
 VALUES ($1, $2, $3, $4, $5);
 
+-- name: GetUserWatchlist :one
+SELECT id, user_id, show_api_id, type, title, image, created_at, updated_at
+FROM watchlists
+WHERE show_api_id = $1 AND user_id = $2 AND deleted_at IS NULL
+    LIMIT 1;
+
+-- name: WatchlistExists :one
+SELECT EXISTS(SELECT 1 FROM watchlists WHERE show_api_id = $1 AND user_id = $2 AND type = $3 AND deleted_at IS NULL);
+
 -- name: GetUserWatchlists :many
-SELECT * FROM watchlists
-WHERE user_id = $1;
+SELECT id, show_api_id, type, title, image, created_at, updated_at
+FROM watchlists
+WHERE user_id = $1 AND deleted_at IS NULL;
 
 -- name: GetUserWatchlistsWithType :many
-SELECT * FROM watchlists
-WHERE user_id = $1 AND type = $2;
+SELECT id, show_api_id, type, title, image, created_at, updated_at
+FROM watchlists
+WHERE user_id = $1 AND type = $2 AND deleted_at IS NULL;
 
 -- name: DeleteWatchlist :exec
-DELETE FROM watchlists
-WHERE show_api_id = $1 AND user_id = $2;
+UPDATE watchlists
+SET deleted_at = NOW()
+WHERE show_api_id = $1 AND user_id = $2 AND deleted_at IS NULL;
